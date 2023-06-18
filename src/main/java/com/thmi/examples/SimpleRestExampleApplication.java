293cd8576d4c2c1995ca59a94f8e663d851cac62
Kirill Kostin <kirill.kostin@energymachines.com>
2023-06-18 22:12:11 +0200

    THMI-0 create project

---
+++ src/main/java/com/thmi/examples/SimpleRestExampleApplication.java
@@ -0,0 +1,13 @@
+package com.thmi.examples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleRestExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleRestExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/thmi/examples/SimpleRestExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thmi.examples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleRestExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
